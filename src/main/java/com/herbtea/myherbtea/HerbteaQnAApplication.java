9a2dbb259f65e3a3c0f2668150f5dd43318fcdbe
herbtea <herbtea30@gmail.com>
2020-08-23 00:00:44 +0900

    Initial commit

---
+++ src/main/java/com/herbtea/myherbtea/HerbteaQnAApplication.java
@@ -0,0 +1,13 @@
+package com.herbtea.myherbtea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerbteaQnAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerbteaQnAApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/herbtea/myherbtea/HerbteaQnAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herbtea.myherbtea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerbteaQnAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
