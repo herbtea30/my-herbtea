66de717e607dd475b67cd0d7f0f527f94a2d4632
herbtea <herbtea30@gmail.com>
2020-08-24 23:43:04 +0900

    mustache and mvc function study add feature create User list search

--- src/main/java/com/herbtea/myherbtea/web/UserController.java
+++ src/main/java/com/herbtea/myherbtea/web/UserController.java
@@ -1,13 +1,27 @@
 package com.herbtea.myherbtea.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
+
+    private List<User> users = new ArrayList<>();
+
     @PostMapping("/create")
     public String create(User user) {
-        System.out.println(user);
-        return "index";
+        users.add(user);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("users", users);
+        return "list";
     }
 }
