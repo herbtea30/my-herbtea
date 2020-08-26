d3cd03f70872c5398226d0a16185c5d71ec17c84
herbtea <herbtea30@gmail.com>
2020-08-26 21:36:27 +0900

    mustache tamplate test

--- src/main/java/com/herbtea/myherbtea/web/WelcomeController.java
+++ src/main/java/com/herbtea/myherbtea/web/WelcomeController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WelcomeController {
     @GetMapping("/helloworld")
-    public String welcome(String name, int age, String sex, Model model) {
-        model.addAttribute("name", name);
-        model.addAttribute("age", age);
-        model.addAttribute("sex", sex);
+    public String welcome(Model model) {
+        model.addAttribute("name", "herbta");
+        model.addAttribute("age", 10);
+        model.addAttribute("sex", "male");
+        model.addAttribute("taxed_value", 10);
+        model.addAttribute("in_ca", true);
         return "welcome";
     }
 }
