7aeaffbcd0e2dbe4a3660915dfa29d8088489756
herbtea <herbtea30@gmail.com>
2020-08-26 21:42:20 +0900

    mustache tamplate test list for print

---
+++ src/main/java/com/herbtea/myherbtea/web/MyModel.java
@@ -0,0 +1,13 @@
+package com.herbtea.myherbtea.web;
+
+public class MyModel {
+    private String name;
+
+    public MyModel(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- src/main/java/com/herbtea/myherbtea/web/WelcomeController.java
+++ src/main/java/com/herbtea/myherbtea/web/WelcomeController.java
@@ -4,15 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class WelcomeController {
     @GetMapping("/helloworld")
     public String welcome(Model model) {
+        List<MyModel> models = Arrays.asList(new MyModel("dony"), new MyModel("herbtea"), new MyModel("cheesena"));
         model.addAttribute("name", "herbta");
         model.addAttribute("age", 10);
         model.addAttribute("sex", "male");
         model.addAttribute("taxed_value", 10);
         model.addAttribute("in_ca", true);
+        model.addAttribute("models", models);
         return "welcome";
     }
 }
