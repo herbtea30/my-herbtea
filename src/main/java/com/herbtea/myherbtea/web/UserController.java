613c714f045b1e47f8e6ae356499734b14590dcb
herbtea <herbtea30@gmail.com>
2020-08-30 21:37:18 +0900

    mustache template apply navigation

--- src/main/java/com/herbtea/myherbtea/web/UserController.java
+++ src/main/java/com/herbtea/myherbtea/web/UserController.java
@@ -6,11 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
     //private List<User> users = new ArrayList<>();
@@ -20,15 +22,15 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    @PostMapping("/create")
+    @PostMapping("")
     public String create(User user) {
         userRepository.save(user);
-        return "redirect:/list";
+        return "redirect:/users";
     }
 
-    @GetMapping("/list")
+    @GetMapping("")
     public String list(Model model) {
         model.addAttribute("users", userRepository.findAll());
-        return "list";
+        return "/user/list";
     }
 }
