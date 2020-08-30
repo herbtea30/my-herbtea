97c000fa7121444a9412e91067616e91790f7740
herbtea <herbtea30@gmail.com>
2020-08-30 21:00:07 +0900

    change feature User add and Search Using by H2 Database

--- src/main/java/com/herbtea/myherbtea/web/UserController.java
+++ src/main/java/com/herbtea/myherbtea/web/UserController.java
@@ -1,5 +1,7 @@
 package com.herbtea.myherbtea.web;
 
+import com.herbtea.myherbtea.domain.User;
+import com.herbtea.myherbtea.domain.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,17 +13,22 @@ import java.util.List;
 @Controller
 public class UserController {
 
-    private List<User> users = new ArrayList<>();
+    //private List<User> users = new ArrayList<>();
+    private UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @PostMapping("/create")
     public String create(User user) {
-        users.add(user);
+        userRepository.save(user);
         return "redirect:/list";
     }
 
     @GetMapping("/list")
     public String list(Model model) {
-        model.addAttribute("users", users);
+        model.addAttribute("users", userRepository.findAll());
         return "list";
     }
 }
