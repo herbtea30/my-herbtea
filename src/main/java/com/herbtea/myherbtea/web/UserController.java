92e166e07ab618d6eb203f2ff42f9b6f4bd1a1a8
herbtea <herbtea30@gmail.com>
2020-09-04 23:33:14 +0900

    modify user information using mustache

--- src/main/java/com/herbtea/myherbtea/web/UserController.java
+++ src/main/java/com/herbtea/myherbtea/web/UserController.java
@@ -5,11 +5,13 @@ import com.herbtea.myherbtea.domain.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/users")
@@ -25,6 +27,13 @@ public class UserController {
     @GetMapping("/form")
     public String form() {
         return "/user/form";
+    }
+
+    @GetMapping("/{id}/form")
+    public String updateForm(@PathVariable Long id, Model model ) {
+        Optional<User> user = userRepository.findById(id);
+        model.addAttribute("user", user.get());
+        return "/user/updateForm";
     }
 
     @PostMapping("")
