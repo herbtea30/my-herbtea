f2500280770610407fe5bbe9e390630d3bba5ec7
herbtea <herbtea30@gmail.com>
2020-09-07 22:52:12 +0900

    HiddenHttpFilter Setting and Using Http POST -> PUT

--- src/main/java/com/herbtea/myherbtea/HerbteaQnAApplication.java
+++ src/main/java/com/herbtea/myherbtea/HerbteaQnAApplication.java
@@ -2,12 +2,23 @@ package com.herbtea.myherbtea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class HerbteaQnAApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HerbteaQnAApplication.class, args);
+    }
+
+    /**
+     * HiddenHttpMethodFilter
+     */
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        HiddenHttpMethodFilter filter = new HiddenHttpMethodFilter();
+        return filter;
     }
 
 }
--- src/main/java/com/herbtea/myherbtea/web/UserController.java
+++ src/main/java/com/herbtea/myherbtea/web/UserController.java
@@ -4,10 +4,7 @@ import com.herbtea.myherbtea.domain.User;
 import com.herbtea.myherbtea.domain.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,7 +45,7 @@ public class UserController {
         return "/user/list";
     }
 
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     public String update(@PathVariable Long id, User updateUser) {
         Optional<User> user = userRepository.findById(id);
         user.get().update(updateUser);
