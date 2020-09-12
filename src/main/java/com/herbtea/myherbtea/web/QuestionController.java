37c5225f8f7c9f043ec65a31450fff68ef8b2fc0
herbtea <herbtea30@gmail.com>
2020-09-12 22:17:37 +0900

    qna board list feature

--- src/main/java/com/herbtea/myherbtea/web/QuestionController.java
+++ src/main/java/com/herbtea/myherbtea/web/QuestionController.java
@@ -4,7 +4,9 @@ import com.herbtea.myherbtea.domain.Question;
 import com.herbtea.myherbtea.domain.QuestionRepository;
 import com.herbtea.myherbtea.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -38,5 +40,11 @@ public class QuestionController {
         questionRepository.save(question);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/{id}")
+    public String detail(@PathVariable Long id, Model model) {
+        model.addAttribute("question", questionRepository.findById(id));
+        return "/qna/content";
     }
 }
